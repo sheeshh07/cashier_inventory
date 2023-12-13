1fa86f73adb323591da5059cf042cde841cf74fa
DefinitelyNotAnAssassin <46719110+DefinitelyNotAnAssassin@users.noreply.github.com>
2023-12-14 03:12:50 +0800

    h

--- src/finals_cashier_inventory/User.java
+++ src/finals_cashier_inventory/User.java
@@ -2,6 +2,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/GUIForms/JFrame.java to edit this template
  */
+
+// Hello World
 package finals_cashier_inventory;
 
 import java.sql.ResultSet;
